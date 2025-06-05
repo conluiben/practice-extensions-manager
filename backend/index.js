c55ea3c6007f3ac904aab34ec682ae9b4fb6e45f
CoreData Research <cdresearch@Luiz---CoreDatas-MacBook-Air.local>
2025-06-05 14:09:55 +0800

    feat: api endpoint and cors setup
    - fixed post api endpoint for adding new log
    - configured cors, helmet, xss in express app
    - integrated joi in api endpoint request processing

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { join, dirname } from "path";
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 import { fileURLToPath } from "url";
+import cors from "cors";
+import helmet from "helmet";
+import joi from "joi";
+import xss from "xss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,10 +16,68 @@ const logFile = join(__dirname, "logs.json");
 const adapter = new JSONFile(logFile);
 const db = new Low(adapter, { logs: [] });
 
-const fetchLogs = (req, res, next) => {};
-
 const app = express();
 
+// function for API endpoints
+
+const addLog = async (req, res, next) => {
+  try {
+    const { error: schemaError, value: newLogObject } = joi
+      .object({
+        message: joi.string().trim().required().custom(sanitizeString),
+      })
+      .validate(req.body);
+    if (schemaError) {
+      // error object contains details[] where each details: message, path, type...
+      // TODO: add multiple messages
+      throw createError(
+        "Invalid property values - " + schemaError.details[0].message,
+        400
+      );
+    }
+
+    await db.update(({ logs }) =>
+      logs.push({
+        message: newLogObject["message"],
+        category: "Browser Action",
+        timestamp: new Date().toLocaleString(),
+      })
+    );
+    res.status(200).json({ ...newLogObject, success: true });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const handleError = (err, req, res, next) => {
+  console.error("Error occured: " + err.message);
+  res.status(err.status || 500).json({ message: err.message, success: false });
+};
+
+const createError = (msg, statusCode = 500) => {
+  const newError = new Error(msg);
+  newError.status = statusCode;
+  return newError;
+};
+
+const corsOptions = {
+  origin: "http://localhost:4173",
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  optionsSuccessStatus: 200,
+};
+const helmetOptions = {
+  referrerPolicy: {
+    policy: "origin",
+  },
+};
+
+// xss lib to sanitize/escape html, used in joi.custom
+const sanitizeString = (value) => {
+  return xss(value).trim();
+};
+
+app.use(cors(corsOptions));
+app.use(helmet(helmetOptions));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -23,19 +85,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/log", (req, res) => {
-  res.send("Accessing logs...");
   console.log("Accessing logs...");
+  res.send(db.data.logs);
 });
 
-app.post("/log", async (req, res) => {
-  res.status(200).json({ success: true });
-  await db.update(({ logs }) =>
-    logs.push({
-      message: "Posted a new log",
-      timestamp: new Date().toLocaleString(),
-    })
-  );
-});
+app.post("/log", addLog);
+
+app.use(handleError);
 
 // log handling
 const logSetup = async () => {
@@ -47,7 +103,7 @@ const logSetup = async () => {
     typeof db.data !== "object" || // Not an object
     !Array.isArray(db.data.logs) // logs is not an array
   ) {
-    db.data = defaultData;
+    db.data = { logs: [] };
     await db.write(); // Persist the fix
   }
 
